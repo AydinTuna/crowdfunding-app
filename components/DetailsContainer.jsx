00c9b39af83a9ae868a44f605c162b376c6cddc0
benbaho <aydinbahadirtuna@gmail.com>
2024-01-29 04:15:20 +0300

    margin fix

--- components/DetailsContainer.jsx
+++ components/DetailsContainer.jsx
@@ -9,8 +9,7 @@ function DetailsContainer({ pledgeCampaign }) {
     return (
         <div className='min-h-screen bg-white flex flex-col justify-start items-center'>
             <h1 className="text-3xl text-gray-900 dark:text-white text-center">Welcome to Campaign Details Page</h1>
-            <div className='max-w-md'>
-                <img className="rounded-t-lg" src="" alt="campaign image" />
+            <div className='max-w-md mt-8'>
                 <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{campaign.title}</h3>
                 <h3 className="mb-2 text-xl font-normal tracking-tight text-gray-900">{campaign.campaignDescription}</h3>
                 <div className='mx-6 mt-4 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>
